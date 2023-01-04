132d369cc111a783a34a28d04375f10bf3416677
Anthony Climer <anthonyclimer0925@gmail.com>
2023-01-03 21:50:19 -0500

    I have done the gp and completed it

--- src/components/Details.js
+++ src/components/Details.js
@@ -7,17 +7,39 @@ export default function Details(props) {
   const [details, setDetails] = useState(null)
 
   // 👉 TASK 4 - Create a side effect 🥇 that runs only after first render.
-
+useEffect(()=>{
+  console.log('Effect has occured');
+},[])
   // 👉 TASK 5 - Create a side effect 👻 that runs only after first render
   // and puts a 'click' event handler on document.
   // See what happens if we don't clean up.
+useEffect(()=>{
+  console.log('Adding a silly event listener');
+  const sillyClickListener = () => {
+    console.log('Here is a random number ' + (Math.round(Math.random() * 20)) );
+  }
+  document.addEventListener('click', sillyClickListener);
 
+  return ()=>{
+    console.log('Removing silly listener');
+    document.removeEventListener('click', sillyClickListener);
+  }
+},[])
   // 👉 TASK 6 - Create a side effect 🥵 that runs after every render.
-
+useEffect(()=>{
+  console.log('OH GREAT HEAVENS, WHAT HAVE I DONE!')
+})
   // 👉 TASK 7 - Create a side effect 📲 that runs when a particular variable changes:
   // Whenever props.friendId updates we should trigger a fetch for details of the friend.
   // The URL should end up looking like `http://localhost:4000/friends/1?api_key=xyz`
   // On success, shove the details of the friend in `details` slice of state
+
+useEffect(()=>{
+  axios.get(`${BASE_URL}/friends/${friendId}?api_key=${API_KEY}`)
+    .then(res =>{
+      setDetails(res.data);
+})  .catch(err => console.error(err))
+}, [friendId])
 
   return (
     <div className='container'>
